4df0253727bad5ba1dffd87294df72dff69da98a
superUserT <rantshothabisomail@gmail.com>
2025-02-11 15:04:18 +0200

    added to allowed hosts

--- puddle/settings.py
+++ puddle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j4ippt+3h39u4ontllpc8a(4h&^god(7aicz#@q^sl_(w)2otp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localislekkerproject-production.up.railway.app"]
+ALLOWED_HOSTS = ["http://localislekkerproject-production.up.railway.app/"]
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
@@ -122,10 +122,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
